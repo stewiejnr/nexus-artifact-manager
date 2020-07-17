f8b68519d1b88bc7f598909a876fd622a93c99a6
Ricardo Stewart <rstewart@egovja.com>
2020-07-17 10:44:47 -0500

    Added maven, npm and raw upload methods

--- nam/__main__.py
+++ nam/__main__.py
@@ -3,20 +3,57 @@
 from configparser import ConfigParser
 from importlib import resources # Python 3.7+
 import sys
+import requests
+import os
 
 from nam import upload
 from nam import download
 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+proxies = {
+  "http": None,
+  "https": None,
+}
+
+nexus_format = sys.argv[1]
+auth = (sys.argv[2], sys.argv[3])
+url = sys.argv[4]
+path = sys.argv[5]
+artifact_name = sys.argv[6]
+repository = sys.argv[7]
+directory = sys.argv[8]
+
+def assign_parameters():
+    if nexus_format == '' or nexus_format != 'npm' or  nexus_format != 'nuget' or  nexus_format != 'maven':
+        print("Please specify repository format e.g npm, maven, npm, nuget")
+        return 1
+
+    return 0
+
+
 def main():
     cfg = ConfigParser()
     cfg.read_string(resources.read_text("reader", "config.txt"))
-    url = cfg.get("feed", "url")
+    ##url = cfg.get("feed", "url")
 
-    if true:
-        upload.mvn()
-
+    if assign_parameters():
+        print("Nexus Format entered: ", nexus_format)
+        if nexus_format == 'raw' and len(sys.argv) == 9:
+            upload_raw()
+        elif nexus_format == 'npm' and len(sys.argv)  == 8:
+            upload_npm()
+        elif nexus_format == 'maven' and len(sys.argv) == 9:
+            upload_maven()
+        else:
+            print("Nexus Repository format choosen does not have correct amount of parameters")
+            print("Number of arguments entered: ", len(sys.argv))
     else:
-        download.mvn()
+        print("Incorrect parameters added")
+
 
 if __name__ == "__main__":
     main()
--- nam/upload.py
+++ nam/upload.py
@@ -1,14 +1,86 @@
+## upload.py
 
 def upload_maven():
     """Upload artifact to Maven 2 repository"""
-    print("Upload Maven2 type repository successful")
+    params = (('repository', repository),)
+   
+    finalpath = path + '/' + artifact_name
+    
+    if ('tar.gz' in artifact_name):
+        pass
+    
+    artname_lst = artifact_name.split("-", 10)
+    versionnumber, exten = os.path.splitext(artname_lst[-1])
+    
+    extension = exten
+    
+    actual_artifact_name = '-'.join(artname_lst[:-1])
+
+    files = {
+        'maven2.groupId': (None, directory),
+        'maven2.artifactId': (None, actual_artifact_name),
+        'maven2.version': (None, versionnumber),
+        'maven2.asset1': (artifact_name, open(finalpath, 'rb')),
+        'maven2.asset1.extension': (None, extension),
+        'maven2.generate-pom': (None, True)  ## dont know if this will work
+    }
+
+    response = requests.post(url, params=params, files=files, auth=auth, proxies=proxies, verify=False)
+    if response.status_code == 204:
+        print("Nexus Upload successful")
+        return sys.exit(0)
+    elif response.status_code == 400 and ("release" in repository or "staging" in repository):
+        print("Nexus Upload failed. Please modify version number")
+        return sys.exit(1)
+    else:
+        print("Nexus Upload failed")
+        return sys.exit(1)
+    print("Status code: ",response.status_code)
+
 
 
 def upload_npm():
     """Upload artifact to NPM type repository"""
-    print("Upload NPM type repository successful")
+    params = (('repository', repository),)
+    finalpath = path + '/' + artifact_name
+    files = {
+	  'npm.asset' : (None, finalpath),
+    }
+    response = requests.post(url, params=params, files=files, auth=auth)
+    if response.status_code == 204:
+        print("Nexus Upload successful")
+        return sys.exit(0)
+    else:
+        print("Nexus Upload failed")
+        return sys.exit(1)
+    print("Status code: ",response.status_code)
+
 
 
 def upload_raw():
     """Upload artifact to RAW type repository"""
-    print("Upload RAW type repository successful")
+    params = (('repository', repository),)
+	
+    finalpath = path + '/' + artifact_name
+    print("Uploading to Nexus Repository: "+repository)
+    files = {
+
+	  'raw.directory' : (None, directory),
+	  'raw.asset1' : (None, finalpath),
+	  'raw.asset1.filename': (None, artifact_name)
+    }
+	#raw.assetN : (None, 
+	#raw.assetN.filename
+    #response = requests.post("http://localhost:8081/service/rest/v1/components",params=params, files=files, auth=auth)
+    response = requests.post(url, params=params, files=files, auth=auth)
+    if response.status_code == 204:
+        print("Nexus Upload successful")
+        return sys.exit(0)
+    elif response.status_code == 400 and ("release" in repository or "staging" in repository):
+        print("Nexus Upload failed. Please modify version number")
+        return sys.exit(1)
+    else:
+        print("Nexus Upload failed")
+        return sys.exit(1)
+    print("Status code: ",response.status_code)
+
