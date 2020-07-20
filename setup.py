1bcba2ce1d1db9e3c8aa99aa186dcb5133c409ec
Ricardo Stewart <rstewart@egovja.com>
2020-07-20 09:51:22 -0500

    Bug fix: remove importlib

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="nexus-artifact-manager",
-    version="1.0.0",
+    version="1.0.1",
     description="Upload and Download artifact from Nexus v3+ repository",
     long_description=README,
     long_description_content_type="text/markdown",
