1aac3b95816658bc2a92f3c05e097544f2f05288
Ricardo Stewart <rstewart@egovja.com>
2020-07-20 13:21:13 -0500

    Bug fix: variable undefined

--- nam/upload.py
+++ nam/upload.py
@@ -1,17 +1,18 @@
 ## upload.py
+import __main__
 
 def upload_maven():
     """Upload artifact to Maven 2 repository"""
-    params = (('repository', repository),)
+    params = (('repository', __main__.repository),)
    
-    finalpath = path + '/' + artifact_name
+    finalpath = __main__.path + '/' + __main__.artifact_name
     
-    if ('tar.gz' in artifact_name):
+    if ('tar.gz' in __main__.artifact_name):
         pass
     
-    longname, file_extension = os.path.splitext(artifact_name)
+    longname, file_extension = os.path.splitext(__main__.artifact_name)
     
-    artname_lst = artifact_name.split("-", 10)
+    artname_lst = __main__.artifact_name.split("-", 10)
     
     if file_extension == '.war':
         versionnumber = artname_lst[-2]
@@ -28,15 +29,15 @@ def upload_maven():
 
 
     files = {
-        'maven2.groupId': (None, directory),
-        'maven2.artifactId': (None, actual_artifact_name),
+        'maven2.groupId': (None, __main__.directory),
+        'maven2.artifactId': (None, __main__.actual_artifact_name),
         'maven2.version': (None, versionnumber),
         'maven2.asset1': (artifact_name, open(finalpath, 'rb')),
         'maven2.asset1.extension': (None, extension),
         'maven2.generate-pom': (None, True)  ## dont know if this will work
     }
 
-    response = requests.post(url, params=params, files=files, auth=auth, proxies=proxies, verify=False)
+    response = requests.post(__main__.url, params=params, files=files, auth=__main__.auth, proxies=__main__.proxies, verify=False)
     if response.status_code == 204:
         print("Nexus Upload successful")
         return sys.exit(0)
@@ -52,12 +53,12 @@ def upload_maven():
 
 def upload_npm():
     """Upload artifact to NPM type repository"""
-    params = (('repository', repository),)
-    finalpath = path + '/' + artifact_name
+    params = (('repository', __main__.repository),)
+    finalpath = __main__.path + '/' + __main__.artifact_name
     files = {
 	  'npm.asset' : (None, finalpath),
     }
-    response = requests.post(url, params=params, files=files, auth=auth)
+    response = requests.post(__main__.url, params=params, files=files, auth=__main__.auth)
     if response.status_code == 204:
         print("Nexus Upload successful")
         return sys.exit(0)
@@ -70,20 +71,20 @@ def upload_npm():
 
 def upload_raw():
     """Upload artifact to RAW type repository"""
-    params = (('repository', repository),)
+    params = (('repository', __main__.repository),)
 	
-    finalpath = path + '/' + artifact_name
-    print("Uploading to Nexus Repository: "+repository)
+    finalpath = __main__.path + '/' + __main__.artifact_name
+    print("Uploading to Nexus Repository: "+ __main__.repository)
     files = {
 
-	  'raw.directory' : (None, directory),
+	  'raw.directory' : (None, __main__.directory),
 	  'raw.asset1' : (None, finalpath),
-	  'raw.asset1.filename': (None, artifact_name)
+	  'raw.asset1.filename': (None, __main__.artifact_name)
     }
 	#raw.assetN : (None, 
 	#raw.assetN.filename
     #response = requests.post("http://localhost:8081/service/rest/v1/components",params=params, files=files, auth=auth)
-    response = requests.post(url, params=params, files=files, auth=auth)
+    response = requests.post(__main__.url, params=params, files=files, auth=__main__.auth)
     if response.status_code == 204:
         print("Nexus Upload successful")
         return sys.exit(0)
